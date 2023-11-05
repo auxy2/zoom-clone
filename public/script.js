0a651439872b46a0af9582bcd9a0879137c69f22
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-11-05 20:29:17 +0100

    initial commit

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const myVideo = document.createElement("video");
+const videoGrid = document.getElementById("video-grid");
+console.log(videoGrid);
+
+myVideo.muted = true;
+
+let myVideoStream;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+  });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+
+  videoGrid.append(video);
+};
