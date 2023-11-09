6fdf3a6cc59e1f256e9ba16c7750d70fabe3c3cf
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-11-09 16:54:29 +0100

    Updated

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
+const socket = io("/");
+
 const myVideo = document.createElement("video");
 const videoGrid = document.getElementById("video-grid");
 console.log(videoGrid);
 
 myVideo.muted = true;
-
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  path: "3030",
+});
 let myVideoStream;
 
 navigator.mediaDevices
@@ -14,7 +20,32 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (UserVideoStram) => {
+        addVideoStream(video, UserVideoStram);
+      });
+    });
   });
+
+socket.on("user-connected", () => {
+  connectToNewUser(userId);
+});
+
+peer.on("open", (Id) => {
+  socket.emit("join-room", ROOM_ID, Id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (UserVideoStram) => {
+    addVideoStream(video, UserVideoStram);
+  });
+  console.log("new-user");
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+// const { JSDOM } = require("jsdom");
+const ExpressPeerServer = require("peerjs");
 
 const app = express();
+
 const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
-  res.redirect(`/qiubeconqoipeca`);
+  res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", (req, res) => {
+  console.log(req.params.room);
   res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(3030, () => {
